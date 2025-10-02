bd81126e45d9cdfe8a16e7f0f48c0f1b3e70ed36
Imaryanjha <149727541+Imaryanjha@users.noreply.github.com>
2025-10-03 02:47:38 +0530

    Add files via upload
    Upload Verilog source code

---
+++ 4_bit_comparator_df.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.08.2025 09:57:41
+// Design Name: 
+// Module Name: 4_bit_comparator_df
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 4_bit_comparator_df(
+
+    );
+endmodule
