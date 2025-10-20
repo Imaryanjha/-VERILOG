29a71d56055cebdec319daf75a8c7e29bdd09129
Imaryanjha <149727541+Imaryanjha@users.noreply.github.com>
2025-10-20 07:19:32 +0530

    Update RTL.v

--- RAM/RTL.v
+++ RAM/RTL.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03.10.2025 13:06:38
-// Design Name: 
-// Module Name: RAM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module RAM(
 
